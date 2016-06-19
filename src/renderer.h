99e2449f5a58504f3ee768d65c2428bb41d83b5b
Tomasz Zając <tomezpl@gmail.com>
2016-06-19 19:06:12 +0100

    Added a common header for Renderer classes

---
+++ src/renderer.h
@@ -0,0 +1,10 @@
+// renderer.h
+// Common header file for Renderer classes.
+// Include this file instead of single headers from the "renderer" directory
+
+#ifndef WDE_RENDERER
+#define WDE_RENDERER
+
+#include "renderer/renderer_base.h"
+
+#endif
